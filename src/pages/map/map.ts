97008559756be0e7ddec98c2aad3560d0c72209b
Miguel Otero <miguel.otero@larefactoria.com>
2017-11-30 18:59:48 +0100

    20.12: android platform

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, Platform } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, LatLng } from '@ionic-native/google-maps';
 
 /**
  * Generated class for the MapPage page.
@@ -15,7 +15,7 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent, LatLng } from '@ionic-native/go
   templateUrl: 'map.html',
   providers: [GoogleMaps]
 })
-export class MapPage {
+export class MapPage { 
 
   public map: GoogleMap;
 
@@ -23,18 +23,34 @@ export class MapPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage')
-
-    this.platform.ready().then( () => {
-      this.googleMaps.isAvailable().then(()=>{
-        let element: HTMLElement = document.getElementById('map');
-        this.map = this.googleMaps.create(element);
-        this.map.one(GoogleMapsEvent.MAP_READY).then((data:any) => {
-          // Center map
-          let myPosition: LatLng = new LatLng(41.390295, 2.154007);
-          this.map.animateCamera({target: myPosition, zoom: 10});          
-        })
-      }).catch( () => alert("GoogleMap is not available"));
-    })  
+    this.loadMap();
   }
 
+  loadMap() {
+    this.platform.ready().then( () => {
+      //this.googleMaps.isAvailable().then(()=>{
+        //let element: HTMLElement = document.getElementById('map');
+        //this.map = GoogleMaps.create(element);
+        let mapOptions: GoogleMapOptions = {
+          camera: {
+            target: {
+              lat: 43.0741904,  
+              lng: -89.3809802
+            },
+            zoom: 18,
+            tilt: 30
+          }
+        };
+        
+        this.map = GoogleMaps.create('map', mapOptions);
+        
+        this.map.one(GoogleMapsEvent.MAP_READY).then((data:any) => {
+          console.log('Map is ready!!');
+          // Center map
+          //let myPosition: LatLng = new LatLng(41.390295, 2.154007);
+          //this.map.animateCamera({target: myPosition, zoom: 10});          
+        })
+      //}).catch( () => alert("GoogleMap is not available"));
+    })  
+  }
 }
